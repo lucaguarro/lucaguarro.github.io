d303ad146c6a7792f850c618393608efa2529842
Luca Guarro <luca.guarro@gmail.com>
2017-07-23 11:59:00 -0700

    navigation sidebar opening and closing with some javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("side-nav-closed").style.display = "none";
+    document.getElementById("side-nav-open").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("side-nav-closed").style.display = "block";
+    document.getElementById("side-nav-open").style.display = "none";
+}
